2ba4f99bf1834574a29117509c4a003ae1e93a1a
lo98be <bertol.lollo@gmail.com>
2016-09-16 16:56:30 +0200

    Aggiunta funzioni dell'anno scorso e codice a pagina singola
    prevedo di usare netbeans invece che la ide di ZR, quindi una pagina
    sola è più comoda

--- project.cpp
+++ project.cpp
@@ -1,33 +1,6 @@
-//Begin page Appunti
-/*note eccetera
-
-preferibilmente usare asana o github
-
-
-*/
-//End page Appunti
-//Begin page Definitive (Distanza ecc.)
-float dist(int* a,int* b){
-    return sqrt(mathSquare(a[0]-b[0])+mathSquare(a[1]-b[1])+mathSquare(a[2]-b[2]));
-}
-//End page Definitive (Distanza ecc.)
-//Begin page Muovi
-void muovi(){
-    //riesumare
-}
-//End page Muovi
-//Begin page Ruota
-void ruota(){
-    
-}
-//End page Ruota
 //Begin page main
-struct punto{
-    float x;
-    float y;
-    float z;
-};//mi piacerebbe usarla
 int BoR;// 0 se sfera rossa, 1 se blu
+int fase,sottofase;
 float stato[12];
 float statoAvv[12];
 float vai[3];
@@ -37,44 +10,87 @@ float pos[3];
 float facing[3];
 float debug[7];//7 variabili di debug, da usare
 float zona[4];//posizione e dimensione drop zone
-int fase;
-void init(){
-	for(int i=0;i<7;i++)debug[i]=0;
-	for(int i=0;i<3;i++){
-	    vai[i]=punta[i]=0;
-	}
-	api.getMyZRState(stato);
-	BoR=stato[1]>0?0:1;//capisco quale sfera siamo
-	fase=0;
+
+float dist(float* a,float* b){
+    return sqrt(mathSquare(a[0]-b[0])+mathSquare(a[1]-b[1])+mathSquare(a[2]-b[2]));
 }
 
-void loop(){
-	api.getMyZRState(stato);//ottengo dati attuali
+void muovi(){
+        float d=dist(vai,pos);
+        float vec; mathVecSubtract(vec,vai,pos,3);
+        if (d>0.4) api.setVelocityTarget(vec);
+        else api.setPositionTarget(targetPoint);
+}
+
+void frena(){
+    
+}
+
+void ruota(){
+    api.setAttRateTarget(punta);
+}
+
+bool outOfBounds (float* ptc){
+    if (fabsf(ptc[0])>0.75)
+        return true;
+    if (fabsf(ptc[1])>0.75)
+        return true;
+    if (fabsf(ptc[2])>0.75)
+        return true;
+    return false;
+}
+
+void init(){
+	for(int i=0;i<7;i++)debug[i]=0;
+	for(int i=0;i<3;i++)vai[i]=punta[i]=0;
+	api.getMyZRState(stato);
+	BoR=stato[1]>0?0:1;//capisco quale sfera siamo
+	fase=sottofase=0;
+	game.getItemLoc(vai,0);//temporaneo
+}
+
+void inizio(){
+    api.getMyZRState(stato);//ottengo dati attuali
 	api.getOtherZRState(statoAvv);//ottengo dati avversario
 	for(int i=0;i<3;i++){
 	    pos[i]=stato[i];
 	    facing[i]=stato[i+6];
 	    posAvv[i]=statoAvv[i];
 	} //ottengo posizione e direzione miei e posizione avversario (magari poi anche direzione avversario)
-	switch(fase){
-	    case 0://drop SPSs, i cosi per sapere dov si trova la zhohona
-	        //game.dropSPS();
-	        //fase++;
-	    break;
-	    case 1://getZone
-	        if(!game.getZone(zona)){//se true, carica anche la posizione della sfera in zona
-	            fase--;
-	        }
-	    break;
-	    default:
-	    DEBUG(("ERROR"));
-	}
-	
-	
-	
-	api.setDebug(debug);
+}
+void fine(){
+    api.setDebug(debug);
 	ruota();//ordine di rotazione verso punta[]
 	muovi();//ordine di spostamento verso vai[]
+}
+void loop(){
+    inizio();
+	switch(fase){
+        case 0://drop SPSs, i cosi per sapere dove si trova la zhohona
+            if (sottofase==0 && dist(vai,pos)<0.1){
+                game.dropSPS();
+                sottofase++;
+                game.getItemLoc(vai,1);
+            } else if (sottofase==1 && dist(vai,pos)<0.1){
+                game.dropSPS();
+                sottofase++;
+                game.getItemLoc(vai,2);
+            } else if(sottofase==2 && dist(vai,pos)<0.1){
+                game.dropSPS();
+                sottofase=0;
+                fase++;
+            }
+        break;
+        case 1://getZone
+            if(!game.getZone(zona)){//se true, carica anche la posizione della sfera in zona
+                fase--;
+            }
+        break;
+        default:
+        DEBUG(("ERROR"));
+	}
+	
+	fine();
 }
 
 //End page main
