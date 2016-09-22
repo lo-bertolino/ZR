38779d97a3a14ed49733a8de11b95de0dcba7a0c
lo98be <bertol.lollo@gmail.com>
2016-09-22 10:50:25 +0200

    Aggiunti setV, frena e outOfBounds

--- project.cpp
+++ project.cpp
@@ -7,22 +7,50 @@ float punta[3];
 float posAvv[3];
 float pos[3];
 float facing[3];
+float vel[3];//velocità della sfera
+float speed;//velocità (modulo)
 float debug[7];//7 variabili di debug, da usare
 float zona[4];//posizione e dimensione drop zone
 
 float dist(float* a,float* b){
-    return sqrt(mathSquare(a[0]-b[0])+mathSquare(a[1]-b[1])+mathSquare(a[2]-b[2]));
+    //return sqrt(mathSquare(a[0]-b[0])+mathSquare(a[1]-b[1])+mathSquare(a[2]-b[2]));
+    float v[]={0,0,0}; mathVecSubtract(v, a, b, 3);
+    return mathVecMagnitude(v, 3);
+}
+
+//imposta più facilmente le direzioni (meno errori)
+void setV(float *v,float x,float y,float z){
+    v[0]=x;
+    v[1]=y;
+    v[2]=z;
+}
+void setV(float *v,float *c){
+    v[0]=c[0];
+    v[1]=c[1];
+    v[2]=c[2];
 }
 
 void muovi(){
-        float d=dist(vai,pos);
-        float vec; mathVecSubtract(vec,vai,pos,3);
-        if (d>0.4) api.setVelocityTarget(vec);
-        else api.setPositionTarget(targetPoint);
+    if (oOB(pos))setV(vai,0,0,0);
+    float d=dist(vai,pos);
+    DEBUG(("%f muovi\n",d));
+    if (d<0.2)
+        frena(d);
+    else{
+        DEBUG(("verso @vai\n"));
+        float vec[3]; mathVecSubtract(vec,vai,pos,3);
+        api.setVelocityTarget(vec);
+    }
 }
-
-void frena(){
-    
+//doesnt work
+void frena(float p){
+    DEBUG(("frena\n"));
+    float f[3];
+    p*=10;
+    if(p>1){
+    setV(f,0-vel[0]*p,0-vel[1]*p,0-vel[2]*p);
+    api.setAttitudeTarget(f);}
+    else api.setPositionTarget(vai);
 }
 
 void ruota(){
@@ -50,43 +78,48 @@ void inizio(){
     api.getOtherZRState(statoAvv);//ottengo dati avversario
     for(int i=0;i<3;i++){
         pos[i]=stato[i];
+        vel[i]=stato[i+3];
         facing[i]=stato[i+6];
         posAvv[i]=statoAvv[i];
     }//ottengo posizione e direzione miei e posizione avversario (magari poi anche direzione avversario)
+    speed=mathVecMagnitude(vel,3);
 }
 void fine(){
+    setV(debug,vai);
+    debug[3]=speed;
     api.setDebug(debug);
-	ruota();//ordine di rotazione verso punta[]
-	muovi();//ordine di spostamento verso vai[]
+    ruota();//ordine di rotazione verso punta[]
+    muovi();//ordine di spostamento verso vai[]
 }
 void loop(){
     inizio();
-	switch(fase){
+    switch(fase){
         case 0://drop SPSs, i cosi per sapere dove si trova la zhohona
-            if (sottofase==0 && dist(vai,pos)<0.1){
-                game.dropSPS();
-                sottofase++;
-                game.getItemLoc(vai,1);
-            }
-            else if (sottofase==1 && dist(vai,pos)<0.1){
-                game.dropSPS();
-                sottofase++;
-                game.getItemLoc(vai,2);
-            }
-            else if(sottofase==2 && dist(vai,pos)<0.1){
-                game.dropSPS();
-                sottofase=0;
-                fase++;
-            }
+        if (sottofase==0 && dist(vai,pos)<0.1){
+            game.dropSPS();
+            sottofase++;
+            game.getItemLoc(vai,1);
+        }
+        else if (sottofase==1 && dist(vai,pos)<0.1){
+            game.dropSPS();
+            sottofase++;
+            game.getItemLoc(vai,2);
+        }
+        else if(sottofase==2 && dist(vai,pos)<0.1){
+            game.dropSPS();
+            sottofase=0;
+            fase++;
+        }
         break;
         case 1://getZone
-            if(!game.getZone(zona)){//se true, carica anche la posizione della sfera in zona
-                fase--;
-            }
+        if(!game.getZone(zona)){//se true, carica anche la posizione della sfera in zona
+            fase--;
+        }else {
+            setV(vai,zona);
+        }
         break;
         default:
         DEBUG(("ERROR"));
-	}
-	
-	fine();
+    }
+    fine();
 }
