56c60dad719402eedd27c4515008800d0aaaf569
lo98be <bertol.lollo@gmail.com>
2016-09-22 11:04:43 +0200

    prova modifica progetto con classi

--- main.cpp
+++ main.cpp
@@ -1,25 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class sphere{
+	
+};
 
-/* 
- * File:   main.cpp
- * Author: Lorenzo
- *
- * Created on 22 settembre 2016, 10.16
- */
-
-#include <cstdlib>
-
-using namespace std;
-
-/*
- * 
- */
-int main(int argc,char** argv){
-
-	return 0;
+void init(){
+	
 }
 
+void inizio(){
+	
+}
+void fine(){
+	
+}
+void loop(){
+	inizio();
+	
+	fine();
+}
